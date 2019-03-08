567bf19b51de9b31c37402b015159a5b5fa772d3
Moritz Fink <fink.moritz@gmail.com>
2019-03-08 21:57:08 +0100

    Refactoring

--- app/src/main/java/diceroller/android/example/com/diceroller/MainActivity.kt
+++ app/src/main/java/diceroller/android/example/com/diceroller/MainActivity.kt
@@ -8,11 +8,16 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
+        rollButton = findViewById(R.id.roll_button)
+
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -20,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
